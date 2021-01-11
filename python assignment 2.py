0ad6ae743ca0d7e172103787da9b629b024f311b
Soundarya305 <77265193+Soundarya305@users.noreply.github.com>
2021-01-11 18:47:37 +0530

    Add files via upload

---
+++ python assignment 2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+n=5
+for i in range(n):
+    for j in range(i):
+        print('*', end="")
+    print('')
+    
+for i in range(n,0,-1):
+    for j in range(i):
+        print('*', end='')
+    print('')
+
+
+# In[6]:
+
+
+word= input("input a word to reverse:")
+
+for char in range(len(word)-1, -1,-1):
+    print(word[char], end='')
+print("\n")
+
+
+# In[ ]:
+
+
+
+
